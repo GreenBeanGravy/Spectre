ca99dd2860d0f700f14f9be33f9f3d4f043583ab
GreenBeanGravy <73409904+GreenBeanGravy@users.noreply.github.com>
2023-06-10 18:45:13 -0500

    Updated gui.py to speak the values within text fields and the state of the Debug Window checkbox when they change.

--- lib/gui.py
+++ lib/gui.py
@@ -16,11 +16,12 @@ def create_tkinter_gui(settings, detector):
 
     def apply_settings():
         duration, center_radius, show_detections, confidence = get_current_values(duration_var, center_radius_var, show_detections_var, confidence_var)
+
         settings["General"]["duration_threshold"] = duration
         settings["General"]["center_radius"] = center_radius
         settings["General"]["show_detections_window"] = str(show_detections)
         settings["General"]["confidence_threshold"] = confidence
-    
+
         # Apply Settings when the "Apply" button is pressed
         detector.detection_duration_threshold = float(duration)
         detector.center_radius = int(center_radius)
@@ -57,19 +58,19 @@ def create_tkinter_gui(settings, detector):
     show_detections_var = tk.BooleanVar(value=settings["General"]["show_detections_window"] == "True")
     show_detections_checkbutton = ttk.Checkbutton(frame, variable=show_detections_var)
     show_detections_checkbutton.grid(column=1, row=2, sticky="W")
-
+    
     # Confidence threshold
     confidence_label = ttk.Label(frame, text="Confidence Threshold (0 to 1):")
     confidence_label.grid(column=0, row=3, sticky="W")
     confidence_var = tk.StringVar(value=settings["General"]["confidence_threshold"])
     confidence_entry = ttk.Entry(frame, textvariable=confidence_var)
     confidence_entry.grid(column=1, row=3, sticky="W")
-
+            
     # Apply and Save buttons
     apply_button = ttk.Button(frame, text="Apply", command=apply_settings)
-    apply_button.grid(column=0, row=4, pady=(10, 0), sticky="W")
+    apply_button.grid(column=0, row=6, pady=(10, 0), sticky="W")
     save_button = ttk.Button(frame, text="Save", command=save_and_apply_settings)
-    save_button.grid(column=1, row=4, pady=(10, 0), sticky="W")
+    save_button.grid(column=1, row=6, pady=(10, 0), sticky="W")
 
     duration_label.bind('<Enter>', lambda e: speaker.speak("Duration Threshold (0 to 1)"))
     center_radius_label.bind('<Enter>', lambda e: speaker.speak("Center Radius (in pixels)"))
@@ -90,5 +91,24 @@ def create_tkinter_gui(settings, detector):
     confidence_entry.configure(takefocus=1)
     apply_button.configure(takefocus=1)
     save_button.configure(takefocus=1)
+
+    # Define variables for speaking the values of elements when they change
+    def speak_duration(*args):
+        speaker.speak(f"{duration_var.get()}")
+
+    def speak_center_radius(*args):
+        speaker.speak(f"{center_radius_var.get()}")
+
+    def speak_confidence(*args):
+        speaker.speak(f"{confidence_var.get()}")
+
+    def speak_show_detections(*args):
+        speaker.speak(f"{'checked' if show_detections_var.get() else 'not checked'}")
+ 
+    # Use trace to activate whenever a var changes
+    show_detections_var.trace('w', speak_show_detections)
+    duration_var.trace('w', speak_duration)
+    center_radius_var.trace('w', speak_center_radius)
+    confidence_var.trace('w', speak_confidence)
 
     root.mainloop()
